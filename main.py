d2a9aef7c53f2795143ed93ec457280804ea6cbc
migueldsw <miguel_dsw@hotmail.com>
2016-05-08 01:52:00 -0300

    dissimilarityMatrix(dataset) function implementation

--- main.py
+++ main.py
@@ -1,20 +1,52 @@
 import numpy as np
+import scipy as sc
+from scipy.spatial import distance as dist
 from datasets import DATA
+
+dist = sc.spatial.distance.euclidean #set global function 'dist' as the euclidean distance, from scipy implementation
 
 DATASETNAMES = ['fac','fou','kar','mor','pix','zer']
 
 xfac,yfac = DATA["fac"]
 xfou,yfou = DATA["fou"]
+xkar,ykar = DATA["kar"]
 
-print "data loaded..."
+toy = np.array([[1,0,0],[0,1,0],[0,0,0]]) #toy dataset, just for test and validation
+
+print "Multiple Features Data Set loaded..."
 
 def datasetdetails(dataset,name):
 	#print "%s -> instances: %d | dimensions: %d | max/min frst column: %f / %f"%(name,len(dataset), len(dataset[0]), max(dataset[:,0]),min(dataset[:,0]))
 	print "%s -> instances: %d | dimensions: %d "%(name,len(dataset), len(dataset[0]))
 
+def dissimilarityMatrix(dataset): # returns the : D(nxn) from the dataset of n instances:
+	print "...Calculating Dissimilarity Matrix..."
+	mat = []
+	for i in dataset :
+		row = []
+		for j in dataset:
+			row += [dist(i,j)] 
+			#print i, j ,dist(i,j)
+		mat.append(row)
+	mat=np.array(mat)
+	#print mat
+	return mat
+getdm = dissimilarityMatrix # alias
+
+def verifyMatrix(mat): 
+	symmetry = np.allclose(m,np.transpose(m)) #verifies the matrix symmetry
+	zeroDiag = np.allclose(np.diag(m),np.zeros(len(m))) #verifies whether all main diagonal values are equals zero
+	return symmetry and zeroDiag 
+vmat = verifyMatrix #alias
 
 for n in DATASETNAMES:
 	x,y = DATA[n]
 	datasetdetails(x,n)
 
 print("----------------")
+
+
+
+
+
+
