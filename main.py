2c54a02339b897c29f9e999f54eff75c052a3bdb
migueldsw <miguel_dsw@hotmail.com>
2016-05-09 02:57:29 -0300

    starting MVFCMddV initialization

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy as sc
 from scipy.spatial import distance as dist
 from datasets import DATA
+import random as rd 
 
 dist = sc.spatial.distance.euclidean #set global function 'dist' as the euclidean distance, from scipy implementation
 
@@ -19,7 +20,7 @@ def datasetdetails(dataset,name):
 	#print "%s -> instances: %d | dimensions: %d | max/min frst column: %f / %f"%(name,len(dataset), len(dataset[0]), max(dataset[:,0]),min(dataset[:,0]))
 	print "%s -> instances: %d | dimensions: %d "%(name,len(dataset), len(dataset[0]))
 
-def dissimilarityMatrix(dataset): # returns the : D(nxn) from the dataset of n instances:
+def dissimilarityMatrix(dataset,dist): # returns the : D(nxn) from the dataset of n instances:
 	print "...Calculating Dissimilarity Matrix..."
 	mat = []
 	for i in dataset :
@@ -39,11 +40,56 @@ def verifyMatrix(mat):
 	return symmetry and zeroDiag 
 vmat = verifyMatrix #alias
 
-for n in DATASETNAMES:
-	x,y = DATA[n]
-	datasetdetails(x,n)
+#------ MVFCMddV implementation-------
+def getInitialVectorOfMedoidsVector(E,K):
+	G=[]
+	for k in range(K):
+		randIndex = rd.randint(0,len(E)-1)
+		#print yfac[randIndex]
+		g_k = E[randIndex]
+		G.append([g_k])
+	return np.array(G)
+vmv = getInitialVectorOfMedoidsVector #alias
 
-print("----------------")
+def getInitialVectorOfRelevanceWeightVectors(K):
+	LAMBDA = []
+	for k in range(K):
+		lambda_k = [1]
+		LAMBDA.append(lambda_k)
+	return np.array(LAMBDA)
+gbgl = getInitialVectorOfRelevanceWeightVectors #alias
+
+def getInitialVectorOfMembershipDegreeVectors(E,K): #eq. (6)
+	U = []
+	n = len(E)
+	for i in range(n):
+		u_i = []
+		for k in range(K):
+			argSum = 0
+			for h in range(K):
+				argSum += (( LAMBDA[k][0] * dist(E[i], G[k][0] ) )/(  LAMBDA[h][0] * dist(E[i], G[h][0] ) + 1e-25 ))
+			u_i_k = ( argSum ** (1./(m-1.)) ) ** -1.
+			u_i.append(u_i_k)
+		U.append(u_i)
+	return np.array(U)
+
+#------ MVFCMddV init
+E = xfou
+K = 10
+m = 1.6
+p = 1
+D1 = dissimilarityMatrix(E,dist)
+G = getInitialVectorOfMedoidsVector(E,K)
+LAMBDA = getInitialVectorOfRelevanceWeightVectors(K)
+U = getInitialVectorOfMembershipDegreeVectors(E,K)
+#--------------------------------------
+
+def checkdata():
+	for n in DATASETNAMES:
+		x,y = DATA[n]
+		datasetdetails(x,n)
+	print("----------------")
+
 
 
 
